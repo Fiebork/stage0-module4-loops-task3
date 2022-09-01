d1a320aa097f2c62bfa611ab9987ba781d1bea07
Bilbo3340 <vladik.elce1@gmail.com>
2022-09-01 09:23:17 +0300

    upt

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        int sum = 0;
+        for (int i = 0; i < String.valueOf(t).length(); i++) {
+            sum += String.valueOf(t).charAt(i);
+        }
+        System.out.println(sum);
+    }
 
+    public static void main(String[] args) {
+        int t = 123;
+        int sum = 0;
+        String s = String.valueOf(t);
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+            sum += (int) s.charAt(i);
+        }
+        System.out.println(sum);
     }
 }
